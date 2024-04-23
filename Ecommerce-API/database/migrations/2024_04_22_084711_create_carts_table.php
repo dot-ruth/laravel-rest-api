df756bcb8503d0a206fca4cac89e87b14f95eff9
SamrawitJarso <156771197+SamrawitJarso@users.noreply.github.com>
2024-04-23 09:57:33 +0300

    Update 2024_04_22_084711_create_carts_table.php

--- Ecommerce-API/database/migrations/2024_04_22_084711_create_carts_table.php
+++ Ecommerce-API/database/migrations/2024_04_22_084711_create_carts_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('product_id');
+            $table->string('quantity'); 
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
