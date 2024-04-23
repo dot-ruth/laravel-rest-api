1e6aab14a1b271d7ddcf74e1085bdbb5d96d062b
SamrawitJarso <156771197+SamrawitJarso@users.noreply.github.com>
2024-04-23 09:47:05 +0300

    Update CartController.php

--- Ecommerce-API/app/Http/Controllers/CartController.php
+++ Ecommerce-API/app/Http/Controllers/CartController.php
@@ -2,64 +2,153 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\cr;
-use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Http\Requests\CartRequest;
 
 class CartController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *      path="/api/cart",
+     *      operationId="getCarts",
+     *      tags={"Carts"},
+     *      summary="Get a list of all carts",
+     *      description="Returns a list of all carts.",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              type="array",
+     *              @OA\Items(ref="#/components/schemas/Cart")
+     *          )
+     *      )
+     * )
      */
     public function index()
     {
-        //
+        return Cart::all();
     }
 
     /**
-     * Show the form for creating a new resource.
+     * @OA\Post(
+     *      path="/api/cart",
+     *      operationId="createCart",
+     *      tags={"Carts"},
+     *      summary="Create a new cart",
+     *      description="Create a new cart.",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/Cart")
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Cart")
+     *      )
+     * )
      */
-    public function create()
+    public function store(CartRequest $request)
     {
-        //
+        Cart::create($request->all());
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Get(
+     *      path="/api/cart/{id}",
+     *      operationId="getCartById",
+     *      tags={"Carts"},
+     *      summary="Get a cart by ID",
+     *      description="Returns a cart by its ID.",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Cart ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Cart")
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Cart not found"
+     *      )
+     * )
      */
-    public function store(Request $request)
+    public function show(Cart $cart)
     {
-        //
+        return $cart;
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Put(
+     *      path="/api/cart/{id}",
+     *      operationId="updateCart",
+     *      tags={"Carts"},
+     *      summary="Update a cart",
+     *      description="Update a cart by its ID.",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Cart ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/Cart")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Cart")
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Cart not found"
+     *      )
+     * )
      */
-    public function show(cr $cr)
+    public function update(CartRequest $request, Cart $cart)
     {
-        //
+        return $cart->update($request->all());
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * @OA\Delete(
+     *      path="/api/cart/{id}",
+     *      operationId="deleteCart",
+     *      tags={"Carts"},
+     *      summary="Delete a cart",
+     *      description="Delete a cart by its ID.",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Cart ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="Successful operation"
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Cart not found"
+     *      )
+     * )
      */
-    public function edit(cr $cr)
+    public function destroy(Cart $cart)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, cr $cr)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(cr $cr)
-    {
-        //
+        $media->delete();
+        return 'Media successfully deleted.';
     }
 }
